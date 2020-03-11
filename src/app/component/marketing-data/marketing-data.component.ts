b9b0f6f79e87ccc272cdd9798e8fbe434d7d5a27
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-03-11 18:15:40 +0530

    koushik

--- src/app/component/marketing-data/marketing-data.component.ts
+++ src/app/component/marketing-data/marketing-data.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+// import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-marketing-data',
@@ -12,4 +13,21 @@ export class MarketingDataComponent implements OnInit {
   ngOnInit() {
   }
 
+  // value = [
+  // {title:'Chief Executive Officer', listCount:4334571},
+  // {title:'Chief Financial Officer', listCount:46306},
+  // {title:'Chief Marketing Officer', listCount:21051},
+  // {title:'Chief Operating Officer', listCount:12871},
+  // {title:'Chief Technology Officer', listCount:27471},
+  // {title:'Director', listCount:174538},
+  // {title:'Executive Director', listCount:138687},
+  // {title:'Executive Officer', listCount:101718},
+  // {title:'Executive Vice President', listCount:61176},
+  // {title:'Financial Executive', listCount:208481},
+  // {title:'General Manager', listCount:67159},
+  // {title:'Human Resource Executive', listCount:116318},
+  // {title:'Manager', listCount:2840748}
+  // ]
+
+  // displayedColumns = ['title', 'listCount'];
 }
