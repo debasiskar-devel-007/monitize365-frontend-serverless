a4c6561c7a2e43245937e70ec322da94d8f7f221
koushikbiswas <biswakoushik.influxiq@gmail.com>
2020-03-13 09:42:56 +0530

    koushik

--- src/app/component/geo-fence/geo-fence.component.ts
+++ src/app/component/geo-fence/geo-fence.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaService } from '@ngx-meta/core';
 
 @Component({
   selector: 'app-geo-fence',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeoFenceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly meta: MetaService) {
+    this.meta.setTitle('Monitize365 - GeoFence');
+    this.meta.setTag('og:description', 'Contact us TODAY and submit your queries and questions, and our Support Team at Monitize365 would be happy to have them answered for you as soon as we can.');
+    this.meta.setTag('twitter:description', 'Contact us TODAY and submit your queries and questions, and our Support Team at Monitize365 would be happy to have them answered for you as soon as we can.');
+
+    this.meta.setTag('og:keyword', 'Monitize365 Contact Us, Contact Monitize365, Monitize365 Support');
+    this.meta.setTag('twitter:keyword', 'Monitize365 Contact Us, Contact Monitize365, Monitize365 Support');
+
+    this.meta.setTag('og:title', 'Monitize365 - GeoFence');
+    this.meta.setTag('twitter:title', 'Monitize365 - GeoFence');
+    this.meta.setTag('og:type', 'website');
+    this.meta.setTag('og:image', '../../assets/images/logo-fb.jpg');
+    this.meta.setTag('twitter:image', '../../assets/images/logo-twitter.jpg');
+   }
 
   ngOnInit() {
   }
